f7a7064ccf0365dfe05b839976298a82bc4e4473
ParadoxZW <964730078@qq.com>
2020-04-17 13:04:03 +0800

    fix bugs

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 cd nerd
 python train.py --gpu='1,0' \
- --seed=888 --batch_size=$bs \
- --lr=$lr --lrr=$(echo "$lrr * 0.1"|bc) --lr_decay=$ld \
- --b1=0.9 --b2=$b \
+ --seed=888 --batch_size=24 \
+ --lr=3e-5 --lrr=0.6 --lr_decay=0.3 \
+ --b1=0.9 --b2=0.99 \
  --epochs=5 --decay_epoch=3
 cd ..
